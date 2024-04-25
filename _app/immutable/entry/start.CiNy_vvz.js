5023937ab0c442578d46b1534e631fc633663fc8
Vulae <Vulae@users.noreply.github.com>
2024-04-25 01:24:58 +0000

    deploy: 945a543939002ee9551c153bfc7bb6e36a5afeac

---
+++ _app/immutable/entry/start.CiNy_vvz.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.DVS6iXKf.js";export{t as start};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhcnQuQ2lOeV92dnouanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
