74e4297ed10a6202bc49fc246dfc68ae26929989
Vulae <Vulae@users.noreply.github.com>
2024-04-29 05:47:59 +0000

    deploy: b814bbdfbe8a77b4d40b122c0b191d6371c00ff6

---
+++ _app/immutable/entry/start.Cfja5xyw.js
@@ -0,0 +1,2 @@
+import{a as t}from"../chunks/entry.D6lAu4ko.js";export{t as start};
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RhcnQuQ2ZqYTV4eXcuanMiLCJzb3VyY2VzIjpbXSwic291cmNlc0NvbnRlbnQiOltdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiIn0=
