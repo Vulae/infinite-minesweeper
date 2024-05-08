2da7136d320d8976f7168493d376f20e4a319c6d
Vulae <potatocrazy42069@gmail.com>
2024-05-08 17:54:38 -0500

    inset git hash on build

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,30 @@
 <script lang="ts">
     import GithubCorner from "$components/GithubCorner.svelte";
     import "../style.scss";
+    import * as gitinfo from "../gitinfo";
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        let messages: string[] = [];
+        let styles: string[] = [];
+        function newMessage(message: string): void {
+            messages.push(message);
+            styles.push(
+                styles.length % 2 == 0 ?
+                    `padding: 15px 20px; background-color: #3C2F55;` :
+                    `padding: 15px 20px; background-color: #21222C;`
+            );
+        }
+
+        newMessage(`https://github.com/Vulae/infinite-minesweeper`);
+
+        const git = Object.fromEntries(Object.entries(gitinfo).filter(([key, value]) => key != 'PLACEHOLDER'));
+        if(typeof git.HASH == 'string') {
+            newMessage(`${git.HASH}`);
+        }
+
+        console.log(`\n${messages.map(m => `%c${m}`).join('')}%c \n`, ...styles, 'background-color: transparent');
+    });
 
 </script>
 
