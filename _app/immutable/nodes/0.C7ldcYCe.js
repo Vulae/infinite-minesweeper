0c12cd064d1f2ed7a732023e8acf4919d3739a2b
Vulae <Vulae@users.noreply.github.com>
2025-10-14 18:49:40 +0000

    deploy: b4a08eba911cae7e1804ca9416712d36ee183040

---
+++ _app/immutable/nodes/0.C7ldcYCe.js
@@ -0,0 +1,18 @@
+import"../chunks/DsnmJJEf.js";import{f as s,a as B,t as m,b as i,s as v,d as E,g as Q,aH as w,aI as T,aJ as J}from"../chunks/Cq3X6Yav.js";import{s as d,a as f}from"../chunks/BixDsddQ.js";import{p as n,i as D}from"../chunks/CUaiMl2l.js";const H=!0,I=!0,N=Object.freeze(Object.defineProperty({__proto__:null,prerender:H,ssr:I},Symbol.toStringTag,{value:"Module"})),y="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAYAAADDPmHLAAAAAXNSR0IArs4c6QAAAflJREFUeJzt3TGu00AUQFGTEDkSPRW7YG8sjH38noaOio4mUhQlsIUx+pbj3HPqUTJyrqZ4GjnTBAAAAADAi/uw9Qa29v3rl7+ja7+9/Rpa92NHz/Ww9QbYlgDiBBAngDgBxAkgTgBxAogTQNzHrTewhiXTvbfB6d40TdOf/9rNc3MCxAkgTgBxAogTQJwA4gQQJ4A4AcQJIG5Xo+DREe+S8e5pwfffF6zdCydAnADiBBAngDgBxAkgTgBxAogTQJwA4jYfBa9xg3fJePe2YO2nBWv3wgkQJ4A4AcQJIE4AcQKIE0CcAOIEELfKJHDJdO/nChc4l0z3lkwNl6zdCydAnADiBBAngDgBxAkgTgBxAogTQJwA4nbz50bP4PM0DY24f+/ouToB4gQQJ4A4AcQJIE4AcQKIE0CcAOIEELebkeWTGL3tvJvn6gSIE0CcAOIEECeAOAHECSBOAHECiNv8TaF7Ms/z0Lrr9bryTt6PEyBOAHECiBNAnADiBBAngDgBxAkgTgBxu7m8uJZ5nodfa3u7jb2E9nw+D3//5XLZ9DdwAsQJIE4AcQKIE0CcAOIEECeAOAHECSDuJW8Fn06n4fHu8Xgc/tzR2773+334Mw+Hw/BeH4/Hu4+NnQBxAogTQJwA4gQQJ4A4AcQJIE4AAAAAAADw2v4BHFoz5lfKisEAAAAASUVORK5CYII=";var x=s(`<style>.github-corner:hover .octo-arm {
+            animation: octocat-wave 560ms ease-in-out;
+        }
+
+        @keyframes octocat-wave {
+            0%,
+            100% {
+                transform: rotate(0);
+            }
+            20%,
+            60% {
+                transform: rotate(-25deg);
+            }
+            40%,
+            80% {
+                transform: rotate(10deg);
+            }
+        }</style>`),_=s('<!> <a class="github-corner absolute top-0 right-0 z-[9999] border-0" aria-label="View source on GitHub" title="View source on GitHub"><svg width="80" height="80" viewBox="0 0 250 250" aria-hidden="true"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a>',1);function O(l,t){let r=n(t,"animation",3,!0),A=n(t,"newTab",3,!0),g=n(t,"colorBg",3,"#151513"),c=n(t,"colorFg",3,"#FFF");var a=_(),C=B(a);{var b=e=>{var p=x();i(e,p)};D(C,e=>{r()&&e(b)})}var o=v(C,2);d(o,"",{},{"clip-path":"polygon(0% 0%, 100% 0%, 100% 100%, 0% 0%)"});var h=E(o);let u;Q(o),m(e=>{f(o,"href",t.url),f(o,"target",A()?"_blank":"_self"),u=d(h,"",u,e)},[()=>({color:c(),fill:g()})]),i(l,a)}var k=s('<link rel="icon"/>'),L=s("<!> <!>",1);function j(l,t){var r=L();w(c=>{var a=k();m(()=>f(a,"href",y)),i(c,a)});var A=B(r);O(A,{url:"https://github.com/Vulae/infinite-minesweeper"});var g=v(A,2);T(g,()=>t.children??J),i(l,r)}export{j as component,N as universal};
